9d5a483b11ca9477be4e8ed1ed30bc4fa5c8f287
ibukamshindi <ibukamshindi@gmail.com>
2020-09-19 18:46:28 +0300

    intialize extension configure  SQLAlchemy

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import config_options
+
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # ....
+
+    # Initializing flask extensions
+    # bootstrap.init_app(app)
+    db.init_app(app)
