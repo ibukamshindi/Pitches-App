5deeefd596c0fc22d4e55b0b27f2825a79357ad0
ibukamshindi <ibukamshindi@gmail.com>
2020-09-20 06:30:51 +0300

    install uploads extension and intialise it

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     app = Flask(__name__)
 
-    # ....
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
     # Initializing flask extensions
-    # bootstrap.init_app(app)
+    bootstrap.init_app(app)
     db.init_app(app)
-    bootstrap = Bootstrap(app)
+    # configure UploadSet
+    configure_uploads(app,photos)
+    
--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     General configuration parent class
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://patrick:2016@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
 
