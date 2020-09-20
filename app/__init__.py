3a3de24c3fd33e08dd33fb2de2207751c788344c
ibukamshindi <ibukamshindi@gmail.com>
2020-09-20 07:07:16 +0300

    add mail amd login extensions flask

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+login_manager = LoginManager()
 photos = UploadSet('photos',IMAGES)
+mail = Mail()
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,4 +24,8 @@ def create_app(config_name):
     db.init_app(app)
     # configure UploadSet
     configure_uploads(app,photos)
+    mail.init_app(app)
+    login_manager.init_app(app)
+    login_manager.session_protection = 'strong'
+    login_manager.login_view = 'auth.login'
     
