bad98af8c6f320f558e3bda18a1684c4dfa5a71d
ibukamshindi <ibukamshindi@gmail.com>
2020-09-22 11:35:54 +0300

    import flask security

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class User(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
 
